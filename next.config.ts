073584770ffce8bd0d370f81e6441823ab2169cb
Thiago Baptista <thiago@syncmob.com.br>
2024-11-23 18:34:56 -0300

    Add styling base

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  sassOptions: {
+    logger: {
+      warn: function (message: string) {
+        if (message.includes("deprecat")) return;
+        console.warn(message);
+      },
+      debug: function (message: string) {
+        if (message.includes("deprecat")) return;
+        console.log(message);
+      },
+    },
+  },
 };
 
 export default nextConfig;
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
+import Header from "../components/header";
+import "./page.scss";
 
-function Dashboard() {
+const block = "dashboard-page";
+const Dashboard = () => {
   return (
-    <div>
+    <div className={block}>
+      <Header />
       <h1>Dashboard</h1>
       <p>Welcome to the dashboard page!</p>
     </div>
   );
-}
+};
 
 export default Dashboard;
