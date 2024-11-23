6739f5f9c7dccc2c7c0ba602074dbfbf1dda50fe
Thiago Baptista <thiago@syncmob.com.br>
2024-11-23 13:53:02 -0300

    Add storybook + Add basic routing

---
+++ .storybook/main.ts
@@ -0,0 +1,17 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    "@storybook/nextjs",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  staticDirs: ["../public"],
+};
+export default config;
---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,14 @@
+import Button from "@/app/components/button";
+import { StoryFn } from "@storybook/react";
+
+export default {
+  title: "Components/Button",
+  component: Button,
+};
+
+const Template: StoryFn = (args: any) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  children: "Click Me",
+};
